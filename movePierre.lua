77f9ac7d721b16d116bc305b0fe8d2709a6841c2
Cyril BRIAND <briand@et.esiea.fr>
2020-04-10 12:43:49 +0200

    ADD: pierre is now saved, destroyed then reborn

--- movePierre.lua
+++ movePierre.lua
@@ -10,6 +10,64 @@ function getDistance(unit1, unit2)
 	return (xDiff^2 + yDiff^2 + zDiff^2)^0.5 
 end
 
+
+function getTableGroup(groupParam,pointPosition)
+
+    local groupData = {
+    ["visible"] = false,
+    ["taskSelected"] = true,
+    ["route"] = 
+    {
+    }, -- end of ["route"]
+    ["groupId"] = groupParam.getID(groupParam),
+    ["tasks"] = 
+    {
+    }, -- end of ["tasks"]
+    ["hidden"] = false,
+    ["units"] = 
+    {
+        -- [1] = 
+        -- {
+            -- ["type"] = "LAV-25",
+            -- ["transportable"] = 
+            -- {
+                -- ["randomTransportable"] = false,
+            -- }, -- end of ["transportable"]
+            -- ["unitId"] = 2,
+            -- ["skill"] = "Average",
+            -- ["y"] = 616314.28571429,
+            -- ["x"] = -288585.71428572,
+            -- ["name"] = "Ground Unit1",
+            -- ["playerCanDrive"] = true,
+            -- ["heading"] = 0.28605144170571,
+        -- }, -- end of [1]
+    }, -- end of ["units"]
+    ["y"] = pointPosition.y,
+    ["x"] = pointPosition.x,
+    ["name"] = groupParam.getName(groupParam),
+    ["start_time"] = 0,
+    ["task"] = "Ground Nothing",
+  } -- end of [1]
+  
+    for index, unit in pairs(groupParam:getUnits()) do
+        groupData["units"][index] = {}
+        groupData["units"][index]["type"] = groupParam:getUnit(index).getTypeName(unit)
+        groupData["units"][index]["y"] = groupParam:getUnit(index).getPosition(unit).p.z
+        groupData["units"][index]["x"] = groupParam:getUnit(index).getPosition(unit).p.x
+        groupData["units"][index]["transportable"] = 
+            {
+                ["randomTransportable"] = false,
+            } -- end of ["transportable"]
+        groupData["units"][index]["unitId"]=groupParam:getUnit(index).getID(unit)
+        groupData["units"][index]["skill"] = "Average"
+        groupData["units"][index]["name"] = groupParam:getUnit(index).getName(unit)
+        groupData["units"][index]["playerCanDrive"] = true
+        groupData["units"][index]["heading"] = 0.28605144170571
+        
+    end
+	return groupData
+end
+
 pierreUnit = Unit.getByName("pierre") --408m to MI8
 pierreGroup = Unit.getGroup(pierreUnit)
 
@@ -22,13 +80,30 @@ distance = getDistance(pilotMI8Unit,staticCargo)
 trigger.action.outText("Distance to cargo: " .. distance , 15)
 
 
-for index, unit in pairs(pierreGroup:getUnits()) do
-	--trigger.action.outText(Unit.getName(unit) .. "     " .. Unit.getTypeName(unit), 10 )
-	env.info(index .. " LOG OUT " ..Unit.getName(unit))
-	assetUnitPosition =  pierreGroup:getUnit(index).getPosition(unit).p
-	trigger.action.outText("Pierre x" .. assetUnitPosition.x, 15)
-	trigger.action.outText("Pierre y" .. assetUnitPosition.y, 15)
-end
+-- SAVE PIERRE
+	-- pierreGroupTable = getTableGroup(pierreGroup,pierreUnit.getPosition(pierreUnit).p)
+	-- env.info("groupData ".. pierreGroupTable["name"])
+	-- pierreGroup.destroy(pierreGroup)
+
+-- DESTROY PIERRE 
+	-- pierreGroup.destroy(pierreGroup)
+
+-- REBIRTH PIERRE
+	-- coalition.addGroup(country.id.RUSSIA, Group.Category.GROUND, pierreGroupTable)
+
+
+
+
+-- GARBAGE
+
+
+-- for index, unit in pairs(pierreGroup:getUnits()) do
+	-- --trigger.action.outText(Unit.getName(unit) .. "     " .. Unit.getTypeName(unit), 10 )
+	-- env.info(index .. " LOG OUT " ..Unit.getName(unit))
+	-- assetUnitPosition =  pierreGroup:getUnit(index).getPosition(unit).p
+	-- trigger.action.outText("Pierre x" .. assetUnitPosition.x, 15)
+	-- trigger.action.outText("Pierre y" .. assetUnitPosition.y, 15)
+-- end
 -- assetUnitPosition		= pierreGroup:getUnit(1).p
 -- trigger.action.outText("Pierre x" .. assetUnitPosition.x, 15)
 -- trigger.action.outText("Pierre y" .. assetUnitPosition.y, 15)
